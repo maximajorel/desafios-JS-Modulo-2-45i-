23f4c2a51d3928f69b3f78251026eebc51d44336
Máximo Majorel <maximomajorel@gmail.com>
2023-04-19 15:32:16 -0300

    Primera interaccion con DOM

--- 07 Septimo desafio - Reloj con INTL/js/reloj.js
+++ 07 Septimo desafio - Reloj con INTL/js/reloj.js
@@ -6,6 +6,26 @@ let intl = Intl.DateTimeFormat("es-ar", {
 
 setInterval(() => {
   let reloj = intl.format(new Date());
-  // console.log(reloj);
-  document.body.innerHTML = `${reloj}`;
+  console.log(reloj);
+  // document.body.innerHTML = `${reloj}`;
+  document.getElementById("reloj-h3").innerHTML = reloj;
 }, 1000);
+
+function cambiarTema() {
+  let main = document.getElementById("main");
+  let switchButton = document.getElementById("switch-button");
+
+  if (main.classList.contains("contenedor-principal-reloj-oscuro")) {
+    main.classList.remove("contenedor-principal-reloj-oscuro");
+    main.classList.add("contenedor-principal-reloj-claro");
+    switchButton.classList.remove("boton-oscuro");
+    switchButton.classList.add("boton-claro");
+    switchButton.textContent = "Cambiar a tema oscuro";
+  } else {
+    main.classList.remove("contenedor-principal-reloj-claro");
+    main.classList.add("contenedor-principal-reloj-oscuro");
+    switchButton.classList.remove("boton-claro");
+    switchButton.classList.add("boton-oscuro");
+    switchButton.textContent = "Cambiar a tema claro";
+  }
+}
