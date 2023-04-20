2d450bfc9f505523a663e5742c1b34448e36db35
Máximo Majorel <maximomajorel@gmail.com>
2023-04-19 21:58:58 -0300

    Adiccion de titulo, inicio rama dev

--- 07 Septimo desafio - Reloj con INTL/js/reloj.js
+++ 07 Septimo desafio - Reloj con INTL/js/reloj.js
@@ -14,18 +14,24 @@ setInterval(() => {
 function cambiarTema() {
   let main = document.getElementById("main");
   let switchButton = document.getElementById("switch-button");
+  let titulo = document.getElementById("titulo-hora");
 
   if (main.classList.contains("contenedor-principal-reloj-oscuro")) {
     main.classList.remove("contenedor-principal-reloj-oscuro");
     main.classList.add("contenedor-principal-reloj-claro");
     switchButton.classList.remove("boton-oscuro");
     switchButton.classList.add("boton-claro");
+    titulo.classList.remove("title-reloj-oscuro");
+    titulo.classList.add("title-reloj-claro");
     switchButton.textContent = "Cambiar a tema oscuro";
   } else {
     main.classList.remove("contenedor-principal-reloj-claro");
     main.classList.add("contenedor-principal-reloj-oscuro");
     switchButton.classList.remove("boton-claro");
     switchButton.classList.add("boton-oscuro");
+    titulo.classList.remove("title-reloj-claro");
+    titulo.classList.add("title-reloj-oscuro");
+
     switchButton.textContent = "Cambiar a tema claro";
   }
 }
