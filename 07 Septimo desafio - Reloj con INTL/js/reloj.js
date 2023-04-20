fcdcfb93760c11aea295f147bc6606d4566975c8
Máximo Majorel <maximomajorel@gmail.com>
2023-04-19 23:42:05 -0300

    Nuevo diseño reloj

--- 07 Septimo desafio - Reloj con INTL/js/reloj.js
+++ 07 Septimo desafio - Reloj con INTL/js/reloj.js
@@ -8,13 +8,16 @@ setInterval(() => {
   let reloj = intl.format(new Date());
   console.log(reloj);
   // document.body.innerHTML = `${reloj}`;
-  document.getElementById("reloj-h3").innerHTML = reloj;
+  document.getElementById("reloj1-h3").innerHTML = reloj;
+  document.getElementById("reloj2-h3").innerHTML = reloj;
+  document.getElementById("reloj3-h3").innerHTML = reloj;
 }, 1000);
 
 function cambiarTema() {
   let main = document.getElementById("main");
   let switchButton = document.getElementById("switch-button");
   let titulo = document.getElementById("titulo-hora");
+  let reloj2 = document.getElementById("reloj2-h3");
 
   if (main.classList.contains("contenedor-principal-reloj-oscuro")) {
     main.classList.remove("contenedor-principal-reloj-oscuro");
@@ -23,6 +26,10 @@ function cambiarTema() {
     switchButton.classList.add("boton-claro");
     titulo.classList.remove("title-reloj-oscuro");
     titulo.classList.add("title-reloj-claro");
+    reloj2.classList.remove("reloj2");
+    reloj2.classList.add("reloj-2-claro");
+    // reloj2.classList.remove("reloj-2-oscuro");
+    // reloj2.classList.add("reloj2");
     switchButton.textContent = "Cambiar a tema oscuro";
   } else {
     main.classList.remove("contenedor-principal-reloj-claro");
@@ -31,7 +38,8 @@ function cambiarTema() {
     switchButton.classList.add("boton-oscuro");
     titulo.classList.remove("title-reloj-claro");
     titulo.classList.add("title-reloj-oscuro");
-
+    reloj2.classList.remove("reloj-2-claro");
+    reloj2.classList.add("reloj2");
     switchButton.textContent = "Cambiar a tema claro";
   }
 }
