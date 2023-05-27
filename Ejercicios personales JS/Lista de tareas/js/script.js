f0e24279d284f8d61bc1c5c3efc1a97d3cd34880
Máximo Majorel <maximomajorel@gmail.com>
2023-05-27 18:03:34 -0300

    Adicion funcionalidad tareas completadas y cambio de layout elementos

--- Ejercicios personales JS/Lista de tareas/js/script.js
+++ Ejercicios personales JS/Lista de tareas/js/script.js
@@ -2,30 +2,31 @@ const arrayTareas = JSON.parse(localStorage.getItem("arrayTareasLS")) || [];
 const listaTareas = document.getElementById("lista-de-tareas");
 const formularioTareas = document.getElementById("formularioAgregar");
 const botonBorrar = document.getElementById("boton-borrar");
+const tareasCompletadas =
+  JSON.parse(localStorage.getItem("tareas-completadasLS")) || [];
+const ulTareasCompletadas = document.getElementById("tareas-completadas");
 
+// ======================================
+// ===== FUNCIONES =====
 function mostrarTareas(arrayTareas) {
   listaTareas.innerHTML = "";
   arrayTareas.forEach(function (tarea, indice) {
     listaTareas.innerHTML += `<li class="elemento-lista">
+    <p class="elemento-lista-texto">
     ${tarea}
-    <button id="boton-borrar" class="btn btn-danger" onclick="borrarTarea(${indice})">
-      <i
-        class="fa-solid fa-trash boton-borrar"
-        style="color: #ffffff"
-      ></i></button></li> <hr class="separador-tareas">`;
+    </p>
+
+      <button id="boton-completar-tarea" class="btn btn-success" onclick="completarTarea(${indice})"><i class="fa-solid fa-check"></i></button>
+       <button id="boton-borrar" class="btn btn-danger" onclick="borrarTarea(${indice})">
+      <i class="fa-solid fa-trash boton-borrar"></i></button></li> <hr class="separador-tareas">`;
   });
 }
-
-formularioTareas.addEventListener("submit", function (evento) {
-  const tarea = evento.target.elements.tarea.value;
-
-  evento.preventDefault();
-  arrayTareas.unshift(tarea);
-  localStorage.setItem("arrayTareasLS", JSON.stringify(arrayTareas));
-  mostrarTareas(arrayTareas);
-  document.getElementById("input-tarea").value = "";
-});
-
+function mostrarTareasCompletadas(arrayTareasCompletadas) {
+  ulTareasCompletadas.innerHTML = "";
+  arrayTareasCompletadas.forEach(function (tareaCompletada, indice) {
+    ulTareasCompletadas.innerHTML += `<li class="elemento-lista"><del><p class="elemento-lista-texto">${tareaCompletada}</p></del><button class="btn btn-danger" onclick="borrarTareaCompletada(${indice})">Quitar tarea completada</button></li><hr>`;
+  });
+}
 function borrarTarea(indice) {
   swal({
     title: "¿Está seguro de borrar esta tarea?",
@@ -47,4 +48,46 @@ function borrarTarea(indice) {
     }
   });
 }
+function completarTarea(indice) {
+  swal({
+    title: "¿Marcar como completada la tarea?",
+    text: "Una vez marcada como completada, no podrá volver a marcarla como pendiente",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+    buttons: ["Cancelar", "Completar"],
+  }).then((willDelete) => {
+    if (willDelete) {
+      tareasCompletadas.unshift(arrayTareas[indice]);
+      arrayTareas.splice(indice, 1);
+      localStorage.setItem("arrayTareasLS", JSON.stringify(arrayTareas));
+      mostrarTareas(arrayTareas);
+      swal("La tarea ha sido completada", { icon: "success" });
+      localStorage.setItem(
+        "tareas-completadasLS",
+        JSON.stringify(tareasCompletadas)
+      );
+      mostrarTareasCompletadas(tareasCompletadas);
+    }
+  });
+}
+function borrarTareaCompletada(indice) {
+  tareasCompletadas.splice(indice, 1);
+  localStorage.setItem(
+    "tareas-completadasLS",
+    JSON.stringify(tareasCompletadas)
+  );
+  mostrarTareasCompletadas(tareasCompletadas);
+}
+formularioTareas.addEventListener("submit", function (evento) {
+  const tarea = evento.target.elements.tarea.value;
+
+  evento.preventDefault();
+  arrayTareas.unshift(tarea);
+  localStorage.setItem("arrayTareasLS", JSON.stringify(arrayTareas));
+  mostrarTareas(arrayTareas);
+  document.getElementById("input-tarea").value = "";
+});
+
 mostrarTareas(arrayTareas);
+mostrarTareasCompletadas(tareasCompletadas);
